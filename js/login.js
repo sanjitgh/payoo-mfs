84e1327fb6b81a3d13e69f14e3aeac4307d5e315
Sanjit <sanjitkumarghosh0@gmail.com>
2024-09-20 10:01:51 +0600

    ui

--- js/login.js
+++ js/login.js
@@ -1,9 +1,30 @@
-// button click and set event handeler
+// // button click and set event handeler
+// document.getElementById('login-button')
+// .addEventListener('click', function (event) {
+//     event.preventDefault();  // browser loading hobe na 
+//     // phone number select
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber   = document.getElementById('pin-number').value;
+    
+//     // temporary test
+//     if(phoneNumber === '0' && pinNumber === '1234'){
+//         console.log('You are login');
+//     }
+//     else{
+//         alert('Wrong phone number or pin')
+//     }
+// })
+
 document.getElementById('login-button')
-.addEventListener('click', function (event) {
-    event.preventDefault();  // browser loading hobe na 
-    console.log('login button clicked');
-    // phone number select
+.addEventListener('click', function(event){
+    event.preventDefault()
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber   = document.getElementById('pin-number').value;
+    
+    if(phoneNumber === '014' && pinNumber === '0'){
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('your information is wroang')
+    }
 })
