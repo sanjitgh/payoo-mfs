8f8e955ed8fa37ef84ff501645fedd30ca82e74a
Sanjit <sanjitkumarghosh0@gmail.com>
2024-09-20 10:36:45 +0600

    added money and pin setup

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('hello')
+// add money event handler
+
+// step-1: click the add money button
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event){
+        // prevent page relode / disable reload
+        event.preventDefault();
+        
+        // step-2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    const addPinInput   = document.getElementById('input-pin-number').value;
+    console.log(addMoneyInput, addPinInput)
+})
